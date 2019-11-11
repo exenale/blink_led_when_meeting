9243348664c6fbd59d26e1b8e6191b9587b4c277
Jasmine <jasmine@benchsci.com>
2019-11-10 23:45:44 -0500

    wip, cat keeps walking on keyboard

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/exenale/blink_led_when_meeting
+
+go 1.12
+
+require (
+	github.com/gorilla/mux v1.7.3
+	golang.org/x/net v0.0.0-20191101175033-0deb6923b6d9
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+	google.golang.org/api v0.13.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func get(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "calendar data"}`))
+}
+
+func getEvent(w http.ResponseWriter, r *http.Request) {
+	pathParams := mux.Vars(r)
+	w.Header().Set("Content-Type", "application/json")
+
+	if val, ok := pathParams["deviceID"]; ok {
+
+		deviceID := val
+		w.Write([]byte(fmt.Sprintf(`{"deviceID": %v }`, deviceID)))
+		return
+	}
+	// probably never be hit
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte(`{"message": "need a device id"}`))
+
+}
 
 func main() {
-	fmt.Print("Hello, world.")
+	r := mux.NewRouter()
+
+	api := r.PathPrefix("/api/v1").Subrouter()
+	api.HandleFunc("", get).Methods(http.MethodGet)
+
+	api.HandleFunc("/checkEvents/{deviceID}", getEvent).Methods(http.MethodGet)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
